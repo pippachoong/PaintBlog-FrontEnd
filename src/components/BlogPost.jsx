221a665a50b0f43511d710d3b7bec9a75ab4d182
Dee Pabst <dee@canva.com>
2022-11-10 11:32:16 +1100

    updated BlogPost to collect id from the browsers url and make an axios request to the backend

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,17 +1,45 @@
 import react from 'react';
 import {useState, useEffect} from 'react';
+import {useNavigate, useParams} from 'react-router-dom';
+import axios from 'axios';
 
+let BASE_URL = 'http://localhost:3000';
 
-export default function BlogPost (){
+export default function BlogPost (props){
 
+    const {id} = useParams();
 
+    const [loading, setLoading] = useState(false);
+    const [blogPost, setBlogPost] = useState([]);
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`${BASE_URL}/blogs/${id}`)
+            .then(
+                res => {
+                    setBlogPost(res.data);
+                    setLoading(false);
+                }
+            )
+            .catch(err => {
+                console.warn(`Error`, err);
+                setLoading(false);
+            })
+    }, [])
 
+    console.log(`blogPost is: `, blogPost);
 
     return(
 
         <div>
-            <h1>Blog Post</h1>
+            {
+                loading
+                ?
+                (<div> Loading Blog ... </div>)
+                :
+                (<div>{blogPost[0]._id}</div>)
+            }
         </div>
 
     );
