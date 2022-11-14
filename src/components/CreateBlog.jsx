18e14e427ac9fce97fa63c68ff10ccdb600b94d7
Craig Rolleston <craig.rolleston@gmail.com>
2022-11-14 11:14:49 +1000

    form added to create blog component

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -6,7 +6,54 @@ const BASE_URL = 'http://localhost:3000';
 
 export default function CreateBlog(props) {
 
+    const handleInput = (ev) => {
+        console.log('input changing', ev.target.title);
+    }
+
     return (
-        <div>create new blog</div>
-            );
+
+          <div className="createBlog">
+            <h2>Create a Blog!</h2>
+            
+            <form className="postblogform" onSubmit={handleSubmit} >
+
+                <div>
+                <input className="postbloginput" onChange={handleInput}
+                name="title"
+                type="text"
+                required
+                placeholder='give your PaintBlog post a title'
+                />
+                </div>
+                <div>
+                <input className="postbloginput" onChange={handleInput}
+                name="author"
+                type="text"
+                required
+                placeholder='Author'
+                />
+                </div>
+                <div>
+                <input className="postbloginput" onChange={handleInput}
+                name="image"
+                type="text"
+                required
+                placeholder='Image URL'
+                />
+                </div>
+                <div>
+                <input className="postbloginput" onChange={handleInput}
+                name="content"
+                type="text"
+                placeholder='Your blog goes here'
+                />
+                </div>
+                <div className="postblogbutton">
+                <button>Save blog</button>
+                </div>
+            </form>
+           
+
+          </div>
+        );
 }
