4897e6cce19a996dbf38c59de92e40c64bcca4aa
Dee Pabst <dee@canva.com>
2022-11-22 14:08:58 +1100

    updated BlogPost show page to display authors name

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,14 +1,14 @@
 import react from 'react';
-import {useState, useEffect} from 'react';
-import {useNavigate, useParams} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import moment from 'moment';
 
 let BASE_URL = 'http://localhost:3000';
 
-export default function BlogPost (props){
+export default function BlogPost(props) {
 
-    const {id} = useParams();
+    const { id } = useParams();
 
     const [loading, setLoading] = useState(false);
     const [blogPost, setBlogPost] = useState({});
@@ -22,7 +22,7 @@ export default function BlogPost (props){
                     setBlogPost(res.data);
                     setLoading(false);
                 },
-                
+
             )
             .catch(err => {
                 console.warn(`Error`, err);
@@ -33,73 +33,45 @@ export default function BlogPost (props){
     // console.log(`blogPost is: `, blogPost);
     // console.log('blogPost comment',blogPost.comment)
 
-    console.log('blogPost 1', blogPost);
+    console.log('blogPost authors name', blogPost.author.name);
 
-    return(
+    return (
 
         <div>
             {
                 loading
-                ?
-                (<div> Loading Blog ... </div>)
-                :
-                (
-                <>  
-
-                {
-                    // blogPost.map((r) => 
-                    // <div>
-                    // <p>{r.title}</p>
-                    // {/* <p>{r.author.name}</p> */}
-                    // <img src={r.img}></img>
-                    // <p>{r.content}</p>
-                    // </div>
-                    // )
-                }
-                {/* <h2>{blogPost.title}</h2> */}
-                
-                {/* <h3> */}
-                    {/* {blogPost.author.map((r) => 
-                    <div>{r.name}</div>
-                    )} -  */}
-                    {/* {moment(blogPost.createdAt).format('Do MMMM YYYY,h:mm:ss a')} </h3> */}
-                {/* <img src={blogPost.img} alt="" />
-                <p>{blogPost.content}</p> */}
-                {/* <div>
-                {   
-                    // console.log('blogpost', blogPost)
-                    // console.log('blogpost', blogPost.comment)
-                    blogPost.comment === undefined
-                    ? 
-                    (
-                        <div></div>
-                    )
+                    ?
+                    (<div> Loading Blog ... </div>)
                     :
                     (
-                        <div>
-                            <h3>Comments ({blogPost.comment.length})</h3>
-                            <ul>
+                        <>
+                            <h2>{blogPost.title}</h2>
+                            <h3>{blogPost.author.name} - <em>{moment(blogPost.createdAt).format('DD MMM YY, HH:mm:ss')}</em></h3>
+                            <img src={blogPost.img} alt="" />
+                            <p>{blogPost.content}</p>
                             {
-                                blogPost.comment.map(comment => (
-                                    <li>{comment.text}</li>
-                                ))
+                                blogPost.comment === undefined
+                                    ?
+                                    (
+                                        <div>No Comments</div>
+                                    )
+                                    :
+                                    (
+                                        <div>
+                                            <h3>Comments ({blogPost.comment.length})</h3>
+                                            <ul>
+                                                {
+                                                    blogPost.comment.map(comment => (
+                                                        <li>{comment.text}</li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    )
                             }
-                            </ul>
-                        </div>
+                        </>
                     )
-
-                }
-
-                </div> */}
-                </>  
-                ) 
             }
-        </div>
-
+        </div >
     );
-
-
-
 }
-
-// export default BlogPost;
