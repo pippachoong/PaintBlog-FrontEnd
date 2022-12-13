a73c043089685709730bb82593df5fbddd923b31
Mohamad Dakdouk <mdak2806@gmail.com>
2022-12-13 12:06:43 +1100

    created a canvas, and mouse pressed and location of mouse

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,18 +1,74 @@
 import axios from 'axios';
 import  { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Sketch from 'react-p5';
 
 
 const BASE_URL = 'http://localhost:3000';
 
 export default function CreateBlog(props) {
-
+    
+    
+    const [mousePos, setMousePos] = useState({});
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [img, setImg] = useState('');
     const [content, setContent] = useState('');
 
     const navigatePush = useNavigate();
+
+    
+    // useEffect (() => {
+    //     const handleMouseMove = (event) => {
+
+    //         setMousePos({ x: event.clientX, 
+    //                       y: event.clientY
+    //                     })
+            
+    //     }    
+
+    //     window.addEventListener('mousemove', handleMouseMove);
+    //     return () => {
+    //         window.removeEventListener(
+    //             'mousemove',
+    //             handleMouseMove
+    //         )
+    //     }
+    // }, [])
+   
+    const setup = (p5, canvasParentRef) => {
+
+        const canvas = p5.createCanvas(500, 400).parent(canvasParentRef)
+        
+        canvas.mousePressed((event) => {
+            console.log('clicked on canvas', event)
+        })
+    }
+
+    const draw = p5 => {
+        p5.background(0, 0, 0)
+        p5.ellipse(100, 100, 100)
+        p5.fill(255, 0, 0)
+        
+        if(p5.keyIsDown(p5.SHIFT)){
+            p5.noStroke();
+
+            //mouse
+            const mouseXNormalised = p5.mouseX / p5.windowWidth;
+            const hue = mouseXNormalised * 255
+
+            p5.fill(
+                hue, 
+                255,
+                244
+            );
+            p5.ellipse(p5.mouseX, p5.mouseY, 100,100)
+        }
+    }
+
+    // function draw(){s
+        
+    // }
 
     
     const handleSubmit = (ev) => {
@@ -60,7 +116,7 @@ export default function CreateBlog(props) {
 
           <div className="createBlog">
             <h2>Create a Blog!</h2>
-            
+            <Sketch setup={setup} draw={draw} />
             <form className="postblogform" onSubmit={handleSubmit} >
 
                 <div>
