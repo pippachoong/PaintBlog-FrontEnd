8416668385e0b435c96312870a62be6217a22f99
Mohamad Dakdouk <mdak2806@gmail.com>
2023-02-06 12:50:57 +1100

    dynamically changing color

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -16,6 +16,14 @@ export default function CreateBlog(props) {
     const [img, setImg] = useState('');
     const [content, setContent] = useState('');
 
+    // SETTING UP COLOR STATE
+    const [color, setColor] = useState('red');
+    const [colorModes, setColorModes] = useState({
+    RGB: p5.RGB,
+    HSB: p5.HSB
+    });
+    const [colorMode, setColorMode] = useState(p5.RGB);
+
     // array to save circles
     const [circles, setCircles] = useState([]);
 
@@ -42,11 +50,17 @@ export default function CreateBlog(props) {
    
     const setup = (p5, canvasParentRef) => {
 
-        const canvas = p5.createCanvas(500, 400).parent(canvasParentRef)
+        const canvas = p5.createCanvas(500, 400).parent(canvasParentRef);
         
+        // let color_picker = p5.createColorPicker("green");
+        const colorPicker = p5.createColorPicker(color);
+        colorPicker.input((c) => {
+          setColor(c.hex);
+        });
+
         canvas.mousePressed((event) => {
             console.log('clicked on canvas', event)
-        })
+        });
     }
 
     const draw = p5 => {
@@ -54,21 +68,34 @@ export default function CreateBlog(props) {
         p5.background(0, 0, 0)
         // brush size and shape
         p5.ellipse(100, 100, 100)
-        // fill is the paint brush
-        p5.fill(255, 0, 0)
+        // fill is the paint brush // FILL DYNAMICALLY CHANGES
+        p5.fill(p5.color(color));
+
+        // COLOR MODE SET
+        p5.colorMode(colorMode);
+        // p5.colorMode(color_picker.color());
+
+        // p5.colorMode(p5.HSB, 100);
+        // for (let i = 0; i < 100; i++) {
+        // for (let j = 0; j < 100; j++) {
+        //     p5.stroke(i, j, 100);
+        //     p5.point(i, j);
+        // }
+        // }
         
         if(p5.keyIsDown(p5.SHIFT)){
             p5.noStroke();
+            // noStroke();
 
+            // below function provides the rainbow of colors which need to putelse where to allow users to pick the color for there drawing
+           
+            // describe(`Rainbow gradient from left to right.
+            // Brightness increasing to white at top.`);
             //mouse
             const mouseXNormalised = p5.mouseX / p5.windowWidth;
             const hue = mouseXNormalised * 255
 
-            p5.fill(
-                hue, 
-                255,
-                244
-            );
+            p5.fill(p5.color(color));
             p5.ellipse(
                 p5.mouseX, 
                 p5.mouseY, 
@@ -88,7 +115,7 @@ export default function CreateBlog(props) {
         }
         
         for (const circle of circles){
-            p5.fill( circle.hue, 255, 255 );
+            // p5.fill( p5.color(color));
             p5.ellipse(circle.xPos, circle.yPos, circle.size, circle.size)
         }
     }
@@ -155,7 +182,23 @@ export default function CreateBlog(props) {
 
           <div className="createBlog">
             <h2>Create a Blog!</h2>
-            <Sketch setup={setup} draw={draw} />
+            <div>
+                <div>
+                <label>Color:</label>
+                <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+                </div>
+                <div>
+                <label>Color Mode:</label>
+                <select value={colorMode} onChange={(e) => setColorMode(colorModes[e.target.value])}>
+                    {Object.keys(colorModes).map((key) => (
+                    <option key={key} value={key}>
+                        {key}
+                    </option>
+                    ))}
+                </select>
+                </div>
+                <Sketch setup={setup} draw={draw} />
+            </div>
             <form className="postblogform" onSubmit={handleSubmit} >
 
                 <div>
